6d4f5bad3971580fcaceae68ac8166d5d87c357d
Victor Mayhua <i202123106@cibertec.edu.pe>
2024-04-25 11:26:40 -0500

    Agregando el controlador de Jefe de prestamista con el crud completo

--- src/main/java/com/prestamos/repository/UsuarioRepository.java
+++ src/main/java/com/prestamos/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.prestamos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prestamos.model.Usuario;
@@ -7,4 +9,6 @@ import com.prestamos.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
 	Usuario findByNombres(String nombres);
 	Usuario findByIdUsuario(int idUsuario);
+	
+	List<Usuario> findByIdUsuarioLiderAndEstado(int id, int estado);
 }

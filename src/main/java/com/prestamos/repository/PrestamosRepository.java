74d78d099e61f1b05720a841c562f3c65611940a
Marcos Rosales <gitmarcos@correo.com>
2024-05-04 12:37:23 -0500

    creando la formula de acuerdo a la tasa de interes mensual y cantidad de dias para calcular el monto a pagar

--- src/main/java/com/prestamos/repository/PrestamosRepository.java
+++ src/main/java/com/prestamos/repository/PrestamosRepository.java
@@ -11,12 +11,23 @@ import com.prestamos.model.Prestamos;
 @Repository
 public interface PrestamosRepository extends JpaRepository<Prestamos, Integer> {
 	
-    @Query(value = "SELECT p.duracion_dias, " +
+    /*@Query(value = "SELECT p.duracion_dias, " +
             "ROUND(m.monto1 + (m.monto1 * (p.interes / 100)), 2) AS monto1_calculado, " +
             "ROUND(m.monto2 + (m.monto2 * (p.interes / 100)), 2) AS monto2_calculado, " +
             "ROUND(m.monto3 + (m.monto3 * (p.interes / 100)), 2) AS monto3_calculado, " +
             "ROUND(m.monto4 + (m.monto4 * (p.interes / 100)), 2) AS monto4_calculado, " +
             "ROUND(m.monto5 + (m.monto5 * (p.interes / 100)), 2) AS monto5_calculado, " +
+            "p.interes " + // Agrega la columna "interes" a la consulta
+            "FROM prestamos p " +
+            "INNER JOIN monto m ON p.id_monto = m.id_monto", nativeQuery = true)
+    List<Object[]> obtenerCalculoMontos();*/
+    
+    @Query(value = "SELECT p.duracion_dias, " +
+            "ROUND(m.monto1 + (m.monto1 * (p.interes / 100 / 30) * p.duracion_dias), 2) AS monto1_calculado, " +
+            "ROUND(m.monto2 + (m.monto2 * (p.interes / 100 / 30) * p.duracion_dias), 2) AS monto2_calculado, " +
+            "ROUND(m.monto3 + (m.monto3 * (p.interes / 100 / 30) * p.duracion_dias), 2) AS monto3_calculado, " +
+            "ROUND(m.monto4 + (m.monto4 * (p.interes / 100 / 30) * p.duracion_dias), 2) AS monto4_calculado, " +
+            "ROUND(m.monto5 + (m.monto5 * (p.interes / 100 / 30) * p.duracion_dias), 2) AS monto5_calculado, " +
             "p.interes " + // Agrega la columna "interes" a la consulta
             "FROM prestamos p " +
             "INNER JOIN monto m ON p.id_monto = m.id_monto", nativeQuery = true)
